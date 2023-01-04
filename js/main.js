2702a6a6ebb00c4eb1bd9e0d40984b23a31b6f95
kateryna-nikolaienko <work.nikolaos@gmail.com>
2023-01-04 17:54:10 +0200

    page layout and adaptive

--- js/main.js
+++ js/main.js
@@ -1,23 +1,48 @@
-$(function(){
+$(function () {
 
-$('.carousel__inner').slick({
-	arrows: false, // открючает стрелки перехода (кнопки)
-	dots: true, //это устанавливает дотсы(нижние ориентиры)
-	slidesToShow: 3, //показывает 3 слайда
-	responsive: [
-    {
-      breakpoint: 841,
-      settings: {
-        slidesToShow: 2,
-      }
-    },
-    { 
-    	breakpoint: 601,
-      settings: {
-        slidesToShow: 1,
-    }
-}
+  $('.slider').slick({
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    dots: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1800,
+        settings: {
+          slidesToShow: 4,
+        }
+      },
+      {
+        breakpoint: 1580,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 1220,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 760,
+        settings: {
+          slidesToShow: 1,
+        }
+      },
+
     ],
-});
+  });
+
+  $('.menu__burger').on('click', function () {
+    $('.menu__list').toggleClass('menu__list--active');
+  });
+
+  $('.menu__item-link').on('click', function () {
+    $('.menu__list').removeClass('menu__list--active');
+  });
 
 });
