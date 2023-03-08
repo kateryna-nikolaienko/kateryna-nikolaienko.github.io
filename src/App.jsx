9996ae1444ec3a021a90f7a658c28b34b13ab6ab
kateryna-nikolaienko <work.nikolaos@gmail.com>
2023-03-08 17:45:54 +0200

    LESSON-4: Created a new stateless HomePag class

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import './Reset.css';
 import './Slick.css';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import Header from './components/Header/Header';
-import AboutCompany from './components/Main/AboutCompany';
-import Table from './components/Main/Table';
-import InnerSlider from './components/Main/Slider/InnerSlider';
-import Solutions from './components/Main/Solutions';
-import Footer from './components/Footer/Footer';
-import Introduce from './components/Introduce/Introduce';
+import HomePage from './components/HomePage';
 
 class App extends Component {
   constructor(props) {
@@ -42,40 +34,20 @@ class App extends Component {
     } = this.state;
 
     return (
-      <div className="body">
-        <div className="wrapper">
-
-          <Header nameCompany={nameCompany} headerContent={headerContent} />
-
-          <main className="page">
-
-            <Table />
-
-            <AboutCompany aboutContent={aboutContent} />
-
-            <InnerSlider />
-
-            <Solutions />
-
-          </main>
-
-          <Footer
-            email={email}
-            phone={phone}
-            link={link}
-            location={location}
-            footerCompanyContent={footerCompanyContent}
-            footerAboutContent={footerAboutContent}
-          />
-
-          <Introduce
-            introduceName={introduceName}
-            introduceGithub={introduceGithub}
-            introduceDesign={introduceDesign}
-          />
-
-        </div>
-      </div>
+      <HomePage
+        nameCompany={nameCompany}
+        headerContent={headerContent}
+        aboutContent={aboutContent}
+        footerCompanyContent={footerCompanyContent}
+        footerAboutContent={footerAboutContent}
+        email={email}
+        phone={phone}
+        location={location}
+        link={link}
+        introduceName={introduceName}
+        introduceGithub={introduceGithub}
+        introduceDesign={introduceDesign}
+      />
     );
   }
 }
---
+++ src/components/HomePage.jsx
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Header from './Header/Header';
+import AboutCompany from './Main/AboutCompany';
+import Table from './Main/Table';
+import InnerSlider from './Main/Slider/InnerSlider';
+import Solutions from './Main/Solutions';
+import Footer from './Footer/Footer';
+import Introduce from './Introduce/Introduce';
+
+class HomePage extends Component {
+  render() {
+    const {
+      aboutContent,
+      email,
+      phone,
+      location,
+      footerCompanyContent,
+      footerAboutContent,
+      link,
+      nameCompany,
+      headerContent,
+      introduceName,
+      introduceGithub,
+      introduceDesign,
+    } = this.props;
+
+    return (
+      <div className="body">
+        <div className="wrapper">
+
+          <Header nameCompany={nameCompany} headerContent={headerContent} />
+
+          <main className="page">
+
+            <Table />
+
+            <AboutCompany aboutContent={aboutContent} />
+
+            <InnerSlider />
+
+            <Solutions />
+
+          </main>
+
+          <Footer
+            email={email}
+            phone={phone}
+            link={link}
+            location={location}
+            footerCompanyContent={footerCompanyContent}
+            footerAboutContent={footerAboutContent}
+          />
+
+          <Introduce
+            introduceName={introduceName}
+            introduceGithub={introduceGithub}
+            introduceDesign={introduceDesign}
+          />
+
+        </div>
+      </div>
+    );
+  }
+}
+
+HomePage.propTypes = {
+  aboutContent: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+  phone: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  footerCompanyContent: PropTypes.string.isRequired,
+  footerAboutContent: PropTypes.string.isRequired,
+  link: PropTypes.string.isRequired,
+  nameCompany: PropTypes.string.isRequired,
+  headerContent: PropTypes.string.isRequired,
+  introduceName: PropTypes.string.isRequired,
+  introduceGithub: PropTypes.string.isRequired,
+  introduceDesign: PropTypes.string.isRequired,
+};
+
+export default HomePage;
