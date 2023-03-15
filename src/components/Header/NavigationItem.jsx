d04e7b4b2ea398c3d8702690fc617001583a73ca
kateryna-nikolaienko <work.nikolaos@gmail.com>
2023-03-15 12:03:05 +0200

    LESSON-5 (fixes): Removed index prop from NavigationItem component

--- src/components/Header/NavigationItem.jsx
+++ src/components/Header/NavigationItem.jsx
@@ -3,16 +3,15 @@ import PropTypes from 'prop-types';
 
 class NavigationItem extends Component {
   render() {
-    const { key, href, title } = this.props;
+    const { href, title } = this.props;
     return (
-      <li className="menu__item" key={key}>
+      <li className="menu__item">
         <a className="menu__item-link" href={href}>{title}</a>
       </li>
     );
   }
 }
 NavigationItem.propTypes = {
-  key: PropTypes.number.isRequired,
   href: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
 };
