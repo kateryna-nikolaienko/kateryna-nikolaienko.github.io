efad7b44d504216730b8fe97bb70ead7ce6b06c1
Kateryna Nikolaienko <work.nikolaos@gmail.com>
2023-05-15 14:54:35 +0300

    Issue 32: used the const DARK_THEME in the ContextThemeColor component

--- src/context/ContextThemeColor.jsx
+++ src/context/ContextThemeColor.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { DARK_THEME } from '../constants/Constants';
 
 const ContextThemeColor = React.createContext({
-  theme: 'dark',
+  theme: DARK_THEME,
   toggleTheme: () => {},
 });
 export default ContextThemeColor;
